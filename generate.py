372664f41341b46dd8658cc7a4f7db9457bfe9ea
Antonio Cheong <acheong@student.dalat.org>
2023-03-25 15:38:35 +0800

    Update generate.py

--- generate.py
+++ generate.py
@@ -159,24 +159,12 @@ def main(
 
 
 def generate_prompt(instruction, input=None):
-    if input:
-        return f"""Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
+    return f"""Below is an instruction that describes a task. Write a response that appropriately completes the request.
 
-### Instruction:
+### Title:
 {instruction}
 
-### Input:
-{input}
-
-### Response:
-"""
-    else:
-        return f"""Below is an instruction that describes a task. Write a response that appropriately completes the request.
-
-### Instruction:
-{instruction}
-
-### Response:
+### Body:
 """
 
 
